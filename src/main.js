4f867faa8f846a30d5816cf215568650bbb0923d
Artem Archenkov <r203mail@gmail.com>
2020-05-11 15:10:07 +0400

    parallax..

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
-import "./scripts/Flickity";
 import "./scripts/mobile-menu";
 import "./scripts/skills";
 import "./scripts/slider";
