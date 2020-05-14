984f5124f6bbe6b88cacc7240046b5c2a4cc6bd6
Artem Archenkov <r203mail@gmail.com>
2020-05-14 05:28:11 +0400

    added block reviews in admins panel

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -28,7 +28,7 @@
       .container.about__container
         .about__title
           h2.about__title-text Блок "Обо мне"
-          button.about__btn--add Добавить группу
+          button.btn.btn__secondary.btn__add-group Добавить группу
         .about__content
           ul.about__group
             li.group-about
@@ -41,83 +41,105 @@
               .group-about__line-add
 
     section.works
-      .work-add
-        h2.work-add__title Добавление Работы
-        .work-add__content
-          .work-add__left
-            .upload
-              .upload__desc Перетащите либо загрузите изображение
-              .upload__btn
-                button.btn.btn__primary Загрузить
-          .work-add__right
-            .work-add__work-title
-            .work-add__work-link
-            .work-add__work-desc
-            .work-add__work-tags
-            .work-add__work-btns
-                button.btn.btn__cancel Отменить
-                button.btn.btn__primary Загрузить
-      ul.works__list
-        li.works__item
-          .item-add
-            .item-add__content
-              span.item-add__content-text +
-            .item-add__sign
-              span.item-add__sign-text Добавить работу
-        li.works__item
-          .work
-            .work__image-block
-              img.work__image
-              .tags
-            .work__content
-              h3.work__title
-              .work__desc
-              .work__site-page
-                a.work__link
+      .container.works__container
+        .work-add
+          h2.work-add__title Добавление Работы
+          .work-add__content
+            .work-add__left
+              .upload
+                .upload__desc Перетащите либо загрузите изображение
+                .upload__btn
+                  button.btn.btn__primary Загрузить
+            .work-add__right.form-add-item
+              .form-add-item__row
+                .form-add-item__col
+                  label.form-add-item__label Название
+                  input.form-add-item__input
+              .form-add-item__row
+                .form-add-item__col
+                  label.form-add-item__label Ссылка
+                  input.form-add-item__input
+              .form-add-item__row
+                .form-add-item__col
+                  label.form-add-item__label Описание
+                  textarea.form-add-item__input.form-add-item__textarea
+              .form-add-item__row
+                .form-add-item__col
+                  label.form-add-item__label Добавление тега
+                  input.form-add-item__input
+              .form-add-item__btns
+                  button.btn.btn__secondary.btn__cancel Отменить
+                  button.btn.btn__primary Загрузить
+        ul.works__list
+          li.works__item
+            .item-add
+              .item-add__content
+                span.item-add__content-text +
+              .item-add__sign
+                span.item-add__sign-text Добавить работу
+          li.works__item
+            .work
+              .work__image-block
+                img.work__image(src="https://picsum.photos/300", alt="Artem Archenkov")
+                .tags
+              .work__content
+                h3.work__title Новая работа
+                .work__desc
+                  p Описание работы
+                .work__site-page
+                  a.work__link google.com
               .work__btns
                 button.btn.btn__edit Править
                 button.btn.btn__delete Удалить
-      
+
     section.reviews
-      .review-add
-        h2.review-add__title Добавление отзыва
-        .review-add__conten
-          .review-add__left
-            .review-add__img
-            button.btn.btn__add-photo Добавить фото
-          .review-add__right
-            .review-add__name-occupation
-              .review-add__name
-                label.review-add__label Имя автора
-                  input.review-add__input(type="text", name="name")
-              .review-add__occupation
-                label.review-add__label Титул автора
-                  input.review-add__input(type="text", name="occupation")
-            .review-add__review
-              label.review-add__label Имя автора
-                textarea.review-add__textarea(name="review", cols="30", rows="10")
-            .review-add__btns
-              button.btn.btn__cancel Отменить
-              button.btn.btn__primary Загрузить
-      .reviews__list
-        li.reviews__item
-          .item-add
-            .item-add__content
-              span.item-add__content-text +
-            .item-add__sign
-              span.item-add__sign-text Добавить работу
-        li.reviews__item
-          .review
-            .review__title-block
-              .review__img
-              .review__name-occupation
-                h3.review__name Имя Фамилия
-                .review__occupation Программист
-            .review__content
-              p Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quaerat sunt corporis ex, delectus a recusandae illum suscipit nesciunt, inventore assumenda dolores ab repellendus sed consequatur dicta. Dicta, praesentium quas!
-            .review__btns
-              button.btn.btn__edit Править
-              button.btn.btn__delete Удалить
+      .container.rewiews__container
+        .review-add
+          h2.review-add__title Добавление отзыва
+          .review-add__content
+            .review-add__left
+              .upload-photo
+                .upload-photo__img
+                  img.upload-photo__image(src="../images/icons/user.png", alt="Artem Archenkov")
+                .upload-photo__btn
+                  button.btn.btn__add-photo Добавить фото
+            .review-add__right
+              .review-add__name-occupation
+                .form-add-item__row
+                  .form-add-item__col
+                    label.form-add-item__label Имя автора
+                    input.form-add-item__input
+                  .form-add-item__col
+                    label.form-add-item__label Титул автора
+                    input.form-add-item__input
+              .form-add-item__row
+                .form-add-item__col
+                  label.form-add-item__label Отзыв
+                  textarea.form-add-item__input.form-add-item__textarea
+              .form-add-item__btns
+                  button.btn.btn__secondary.btn__cancel Отменить
+                  button.btn.btn__primary Загрузить
+        ul.reviews__list
+          li.reviews__item
+            .item-add
+              .item-add__content
+                span.item-add__content-text +
+              .item-add__sign
+                span.item-add__sign-text Добавить работу
+          li.reviews__item
+            .review
+              .review__title-block
+                .review__avatar
+                  .avatar
+                    img.avatar__image(src="https://picsum.photos/200", alt="Artem Archenkov")
+                .review__name-occupation
+                  h3.review__name Имя Фамилия
+                  .review__occupation Программист
+              .review__content
+                p Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quaerat sunt corporis ex, delectus a recusandae illum suscipit nesciunt, inventore assumenda dolores ab repellendus sed consequatur dicta. Dicta, praesentium quas!
+              .review__btns
+                button.btn.btn__edit Править
+                button.btn.btn__delete Удалить
         
         
 
@@ -215,6 +237,7 @@ body {
 }
 
 .about {
+  margin-bottom: 60px;
   &__container {
     display: flex;
     flex-direction: column;
@@ -234,7 +257,7 @@ body {
 }
 
 .group-about {
-  border: 1px solid black;
+  box-shadow: 4.096px 2.868px 20px 0px rgba(0, 0, 0, 0.07);
   width: 50%;
   /* убери высоту как контент появится */
   height: 395px; 
@@ -243,4 +266,388 @@ body {
   }
 }
 
+.works {
+  margin-bottom: 60px;
+  &__container {
+    display: flex;
+    flex-direction: column;
+  }
+  &__list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  &__item {
+    width: 33.333%;
+    max-width: 33.333%; 
+    max-height: 560px;
+    &:not(:last-child) {
+      margin-right: 30px;
+    }
+  }
+}
+
+.reviews {
+  margin-bottom: 60px;
+  &__container {
+    display: flex;
+    flex-direction: column;
+  }
+  &__list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  &__item {
+    width: 33.333%;
+    max-width: 33.333%; 
+    max-height: 560px;
+    &:not(:last-child) {
+      margin-right: 30px;
+    }
+  }
+}
+
+.work-add {
+  box-shadow: 4.096px 2.868px 20px 0px rgba(0, 0, 0, 0.07);
+  padding-bottom: 30px;
+  margin-bottom: 30px;
+  &__title {
+    font-size: 18px;
+    font-weight: 600;
+    color: #414c63;
+    padding: 30px 15px;
+    margin: 0 20px;
+    margin-bottom: 50px;
+    border-bottom: 1px solid rgba(31, 35, 45, .15);
+  }
+  &__content {
+    display: flex;
+    flex-direction: row;
+    padding: 0 35px;
+  }
+  &__left {
+    width: 50%;
+    margin-right: 30px;
+  }
+  &__right {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+  }
+}
+
+.review-add {
+  box-shadow: 4.096px 2.868px 20px 0px rgba(0, 0, 0, 0.07);
+  padding-bottom: 30px;
+  margin-bottom: 30px;
+  &__title {
+    font-size: 18px;
+    font-weight: 600;
+    color: #414c63;
+    padding: 30px 15px;
+    margin: 0 20px;
+    margin-bottom: 50px;
+    border-bottom: 1px solid rgba(31, 35, 45, .15);
+  }
+  &__content {
+    display: flex;
+    flex-direction: row;
+    padding: 0 35px;
+  }
+  &__left {
+    width: 30%;
+    margin-right: 30px;
+  }
+  &__right {
+    display: flex;
+    flex-direction: column;
+    width: 70%;
+  }
+}
+
+.upload {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* временное решение - переделать */
+  height: 275px; 
+  /* height: 100%; */
+  max-height: 275px;
+  background-color: #dee4ed;
+  border: 1px dashed rgba(31, 35, 45, .15);
+  text-align: center;
+  line-height: 30px;
+  padding: 0px 90px;
+  &__desc {
+    color: #414c63;
+    font-weight: 600;
+    margin-bottom: 30px;
+  }
+}
+
+.upload-photo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  &__img {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 200px;
+    height: 200px;
+    background-color: #dee4ed;
+    border-radius: 50%;
+    margin-bottom: 30px;
+  }
+  &__image {
+    width: 50%;
+  }
+  &__btn {
+    display: flex;
+    width: 100%;
+    justify-content: center
+  }
+}
+
+.btn {
+  display: inline-flex;
+  align-items: center;
+  &__primary {
+    font-weight: 600;
+    color: #fff;
+    text-transform: uppercase;
+    padding: 19px 50px;
+    border-radius: 50px;
+    background: rgb(20,89,226);
+    background: linear-gradient(90deg, rgba(20,89,226,1) 0%, rgba(63,53,203,1) 100%);
+  }
+  &__cancel {
+    font-weight: 600;
+    color: #383bcf;
+    background: transparent;
+    margin: 0 55px;
+  }
+  &__edit {
+    font-weight: 600;
+    background: transparent;
+    color: #414c63;
+    opacity: .7;
+    &:after {
+      content: '';
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      margin-left: 15px;
+      color: #fff;
+      width: 22px;
+      height: 22px;
+      border-radius: 50%;
+      background: svg-load('pencil.svg', fill=#383bcf, width=100%, height=100%);
+    }
+  }
+  &__delete {
+    font-weight: 600;
+    background: transparent;
+    color: #414c63;
+    opacity: .7;;
+    &:after {
+      content: '';
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      margin-left: 15px;
+      color: #fff;
+      width: 22px;
+      height: 22px;
+      border-radius: 50%;
+      background: svg-load('remove.svg', fill=#c92e2e, width=100%, height=100%);
+    }
+  }
+  &__add-photo {
+    font-weight: 600;
+    color: #383bcf;
+    background: transparent;
+  }
+  &__add-group {
+    font-weight: 600;
+    color: #383bcf;
+    background: transparent;
+    margin: 0 55px;
+    &:before {
+      content: '+';
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      margin-right: 15px;
+      color: #fff;
+      width: 22px;
+      height: 22px;
+      border-radius: 50%;
+      background: rgb(20,89,226);
+      background: linear-gradient(90deg, rgba(20,89,226,1) 0%, rgba(63,53,203,1) 100%);
+    }
+  }
+}
+
+.form-add-item {
+  &__row {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 30px;
+  }
+  &__col {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    &:not(:last-child) {
+      margin-right: 30px;
+    }
+  }
+  &__label {
+    font-weight: 600;
+    color: #414c63;
+    opacity: .5;
+  }
+  &__input {
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #414c63;
+    padding: 20px 0;
+  }
+  &__textarea {
+    margin-top: 20px;
+    border: 1px solid #d9dbe0;
+    resize: none;
+  }
+  &__btns {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+}
+
+.item-add {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 560px;
+  width: 100%;
+  height: 100%;
+  background: rgb(20,89,226);
+  background: linear-gradient(90deg, rgba(20,89,226,1) 0%, rgba(63,53,203,1) 100%);
+  &__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    height: 150px;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    font-size: 72px;
+    font-weight: 100;
+    margin-bottom: 30px;
+  }
+  &__sign {
+    font-size: 18px;
+    font-weight: 600;
+  }
+}
+
+.work {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  box-shadow: 4.096px 2.868px 20px 0px rgba(0, 0, 0, 0.07);
+  &__image-block {
+    width: 100%;
+    height: 40%;
+  }
+  &__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  &__content {
+    width: 100%;
+    height: 100%;
+    padding: 40px 30px;
+  }
+  &__title {
+    color: #414c63;
+    font-size: 18px;
+    font-weight: 600;
+    margin-bottom: 30px;
+  }
+  &__desc {
+    color: #414c63;
+    opacity: .75;
+    font-weight: 600;
+    margin-bottom: 30px;
+  }  
+  &__site-page {
+    color: #4548d2;
+    opacity: .75;
+    font-weight: 600;
+  }
+  &__btns {
+    display: flex;
+    width: 100%;
+    padding: 0 30px;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 30px;
+  }
+}
+
+.review {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+  width: 100%;
+  height: 100%;
+  box-shadow: 4.096px 2.868px 20px 0px rgba(0, 0, 0, 0.07);
+  &__title-block {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    border-bottom: 1px solid rgba(31, 35, 45, .15);
+    padding: 0 10px 30px 10px;
+  }
+  &__avatar {
+      width: 46px;
+  }
+  &__name-occupation {
+    margin-left: 20px;
+  }
+  &__name {
+    font-size: 18px;
+    font-weight: 600;
+    color: #414c63;
+  }
+  &__occupation {
+    font-weight: 600;
+    color: #414c63;
+    opacity: .5;
+  }
+  &__content {
+    flex: 1;
+    font-weight: 600;
+    color: #414c63;
+    opacity: .7;
+    padding: 30px 0;
+  }
+  &__btns {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+}
 </style>
