c540a3db04ae51b5d01c45c85a2c11aee62edf7e
Artem Archenkov <r203mail@gmail.com>
2020-05-14 16:43:03 +0400

    start login page

---
+++ src/login/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .content
+    123
+
+</template>
+
+<style lang="postcss"> 
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
+@import "normalize.css";
+@import "../styles/mixins.pcss";
+@import "../styles/layout/base.pcss";
+
+/* @import "./styles/layout.pcss"; */
+
+
+</style>
